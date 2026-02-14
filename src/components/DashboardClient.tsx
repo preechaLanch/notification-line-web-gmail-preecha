faec8fc72c90623351d7b9f9c6d1214083ab8565
preechaLanch <preecha.lanch@gmail.com>
2026-02-14 08:18:14 +0700

    fix: update DashboardClient modal and logic

--- src/components/DashboardClient.tsx
+++ src/components/DashboardClient.tsx
@@ -125,7 +125,7 @@ export default function DashboardClient({
                 userId: user.id,
                 title: "แจ้งเตือนจากระบบ",
                 message: message,
-                url: "/dashboard",
+                url: "/",
               }),
             });
 
